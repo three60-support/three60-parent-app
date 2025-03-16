eb33ab350996fdb3f954b1989b3ad87a83731dba
three60support <support@three60.in>
2025-03-16 20:22:59 +0530

    Added try catch

--- www/login_form_app.js
+++ www/login_form_app.js
@@ -9,7 +9,8 @@ document.addEventListener ("DOMContentLoaded", function ()	{
 		event.preventDefault								();
 		alert												("On button click.");
 		//localStorage.setItem								(STORAGE_KEY_SERVER_URL, "https://intranet.gdgoenkadwarka.com");
-		alert												("About to post");
+		alert												("About to post " + $("#login_form").serialize());
+		try													{
 		htmlPost											("https://intranet.gdgoenkadwarka.com" + "/servlets/three60.Infrastructure.Login.doLogin", $("#login_form").serialize(),
 			function (login_response_object)				{
 				alert										("Login Success");
@@ -29,6 +30,10 @@ document.addEventListener ("DOMContentLoaded", function ()	{
 			alert											("Error " + response.responseText);
 		});*/
 		alert												("Posted");
+		}
+		catch  (err)										{
+			alert											("Catch Error : " + err.message);
+		}
 	});
 	alert													("After setting listener");
 });
