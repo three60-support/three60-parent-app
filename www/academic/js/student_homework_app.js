0a964f71bf69cf15c4e8a7009f0872b2ac287a06
three60support <support@three60.in>
2025-03-18 11:54:14 +0530

    Wrapped all code in showHomeworkFragment and changed all id variables.

--- www/academic/js/student_homework_app.js
+++ www/academic/js/student_homework_app.js
@@ -1,13 +1,21 @@
-{
+function showHomeworkFragment ()							{
+	const	homework_as_on_element							=  document.querySelector ("#main_frame #homework_as_on");
+	const	get_homework_button_element						=  document.querySelector ("#main_frame #get_homework_button");
+	const	homework_tbody_element							=  document.querySelector ("#main_frame #homework_tbody");
+	const	homework_modal_element							=  document.querySelector ("#main_frame #homework_modal");
+	const	homework_subject_div_element					=  document.querySelector ("#main_frame #homework_subject_div");
+	const	given_on_div_element							=  document.querySelector ("#main_frame #given_on_div");
+	const	due_date_div_element							=  document.querySelector ("#main_frame #due_date_div");
+	const	homework_div_element							=  document.querySelector ("#main_frame #homework_div");
 	const	serverUrl										=  localStorage.getItem (STORAGE_KEY_SERVER_URL);
 	const	loginToken										=  localStorage.getItem (STORAGE_KEY_LOGIN_TOKEN);
-	var  homeworkId;
-	$("#homework_as_on").datepicker							({
+	var		homeworkId;
+	$(homework_as_on_element).datepicker					({
 		format												:  "dd-mm-yyyy",
 		autoclose											:  true,
 		endDate												:  "+0d"
 	}).datepicker											("setDate", 'now');
-	const  homeworkDatatable								=  new DataTable ("#homework_table", {
+	const  homeworkDatatable								=  new DataTable ("#main_frame #homework_table", {
 		columns												:  [
 			{	visible										:  false		},
 			{	title										:  "Subject"	},
@@ -24,12 +32,12 @@
 	});
 
 	_getHomework											();
-	get_homework_button.addEventListener					("click", function (event)	{
+	get_homework_button_element.addEventListener			("click", function (event)	{
 		event.preventDefault								();
 		_getHomework										();
 	});
 
-	homework_tbody.addEventListener ("click", function (event)	{
+	homework_tbody_elememt.addEventListener					("click", function (event)	{
 		const  event_target									=  event.target;
 		if  (event_target)									{
 			if  (event_target.className  ==  "view_homework")	{
@@ -38,7 +46,7 @@
 		}
 	});
 
-	homework_modal.addEventListener ("click", function		(event)		{
+	homework_modal_element.addEventListener					("click", function (event)	{
 		const  event_target									=  event.target;
 		if  (event_target)									{
 			if  (event_target.className  ==  "attachment_link")			{
@@ -50,7 +58,7 @@
 
 	function  _getHomework ()								{
 		htmlPost											(serverUrl + "/servlets/three60.Academic.Homework.GetStudentHomeworkMetaArrays",
-			"homework_as_on=" + homework_as_on.value + "&login_token=" + loginToken,
+			"homework_as_on=" + homework_as_on_element.value + "&login_token=" + loginToken,
 			function  (homework_meta_arrays)				{
 				if  (!rightsIsAuthorised (homework_meta_arrays, FRONTEND_TYPE_APP))		{
 					return;
@@ -87,11 +95,11 @@
 	function _viewHomework (event_target)					{
 		const  homework_data								=  homeworkDatatable.row ($(event_target).parents("tr")).data();
 		homeworkId											=  homework_data [0];
-		homework_subject_div .textContent					=  homework_data [1];
-		given_on_div.textContent							=  homework_data [2];
-		due_date_div.textContent							=  homework_data [3];
-		homework_div.innerHTML								=  homework_data [4];
-		$("#homework_modal").modal							("show");
+		homework_subject_div_element.textContent			=  homework_data [1];
+		given_on_div_element.textContent					=  homework_data [2];
+		due_date_div_element.textContent					=  homework_data [3];
+		homework_div_element.innerHTML						=  homework_data [4];
+		$("#main_frame #homework_modal").modal				("show");
 	}
 
 
