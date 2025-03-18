dc724726c0d4b8a590fa2907508f3a50176d3ae2
three60support <support@three60.in>
2025-03-18 13:49:58 +0530

    Wrapped all code in showEmailFragment and handled id variables.

--- www/communication/js/email_app.js
+++ www/communication/js/email_app.js
@@ -1,20 +1,20 @@
-{
+function showEmailFragment ()								{
 	const  serverUrl										=  localStorage.getItem (STORAGE_KEY_SERVER_URL);
 	const  loginToken										=  localStorage.getItem (STORAGE_KEY_LOGIN_TOKEN);
 
-	$(".datepicker").datepicker								({
+	$("#main_frame .datepicker").datepicker					({
 		format												:  "dd-mm-yyyy",
 		autoclose											:  true,
 		endDate												:  "+0d"
 	});
-	$("#from_date").on ("changeDate", function (selected)	{
-		$("#to_date").datepicker							("setStartDate", new Date (selected.date.valueOf()));
+	$("#main_frame #from_date").on							("changeDate", function (selected)	{
+		$("#main_frame #to_date").datepicker				("setStartDate", new Date (selected.date.valueOf()));
 	});
-	$("#to_date").on ("changeDate", function (selected)		{
-		$("#from_date").datepicker							("setEndDate", new Date (selected.date.valueOf()));
+	$("#main_frame #to_date").on							("changeDate", function (selected)	{
+		$("#main_frame #from_date").datepicker				("setEndDate", new Date (selected.date.valueOf()));
 	});
 
-	const  emailDataTable									=  new DataTable ("#email_table", {
+	const  emailDataTable									=  new DataTable ("#main_frame #email_table", {
 		columns												:  [
 			{	visible										:  false		},
 			{	title										:  "Subject"	},
@@ -29,13 +29,13 @@
 		ordering											:  false
 	});
 
-	get_email_button.addEventListener						("click", function (event)	{
+	$("#main_frame #get_email_button").on					("click", function (event)	{
 		event.preventDefault								();
 		_getEmails											();
 	});
 	_getEmails												();
 
-	email_tbody.addEventListener							("click", function (event)	{
+	$("#main_frame #email_tbody").on						("click", function (event)	{
 		const  event_target									=  event.target;
 		if  (event_target)									{
 			if  (event_target.className  ==  "view_email")	{
@@ -46,7 +46,7 @@
 
 
 	function  _getEmails ()									{
-		$.get												(serverUrl + "/servlets/three60.Communication.Email.GetEmailArrays", $("#get_email_form").serialize() + "&login_token=" +loginToken,
+		$.get												(serverUrl + "/servlets/three60.Communication.Email.GetEmailArrays", $("#main_frame #get_email_form").serialize() + "&login_token=" +loginToken,
 			function (email_arrays)							{
 				emailDataTable.clear().rows.add				(email_arrays).draw();
 			},  "json"
@@ -58,9 +58,9 @@
 		const  email_row_data								=  emailDataTable.row ($(event_target).parents("tr")).data();
 		$.get												(serverUrl + "/servlets/three60.Communication.Email.GetEmailArrays", "mail_id=" + email_row_data[0] + "&login_token=" + loginToken,
 			function  (email_array)							{
-				email_subject_div.textContent				=  email_row_data[1];
-				creation_time_div.textContent				=  email_row_data[2];
-				body_div.innerHTML							=  email_array[0];
+				$("#main_frame #email_subject_div").text	(email_row_data[1]);
+				$("#main_frame #creation_time_div").text	(email_row_data[2]);
+				$("#main_frame #body_div").html				(email_array[0]);
 				$("#email_modal").modal						("show");
 			},  "json"
 		);
