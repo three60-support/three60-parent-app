2ccf0877d27ab12e110fc718f25c8b66144ab92e
three60-support <support@three60.in>
2025-07-10 10:16:39 +0530

    Moved the listener for Pay Button to within _getFeePayable()

--- www/financial/js/fee_parent_app.js
+++ www/financial/js/fee_parent_app.js
@@ -8,17 +8,16 @@ function appShowFeeFragment ()								{
 		$.get												(serverUrl + "/servlets/in.three60.financial.fee.GetFeePayableObject", "login_token=" + loginToken,
 			function  (fee_payable_object)					{
 				$("#main_frame #online_fee_payment_div").html	(Mustache.render ($("#main_frame #online_fee_payment_template").html(), fee_payable_object.data));
+				$("#main_frame #online_fee_payment_div #online_fee_payment_button").on		("click", function (event)	{
+					event.preventDefault							();
+					const  online_fee_payment_browser				=  cordova.InAppBrowser.open (serverUrl + "/servlets/three60.Financial.Fee.OnlineFeePaymentRequest",
+																			$("#main_frame #online_fee_payment_div #online_fee_payment_form").serialize() + "&login_token=" + loginToken, "_blank", "location=no");
+			
+					online_fee_payment_browser.addEventListener		("exit", function ()		{
+						_getFeePayable								();
+					});
+				});
 			},  "json"
 		);
 	}
-
-	$("#main_frame #online_fee_payment_button").on			("click", function (event)	{
-		event.preventDefault								();
-		const  online_fee_payment_browser					=  cordova.InAppBrowser.open (serverUrl + "/servlets/three60.Financial.Fee.OnlineFeePaymentRequest",
-																	$("#main_frame #online_fee_payment_form").serialize() + "&login_token=" + loginToken, "_blank", "location=no");
-
-		online_fee_payment_browser.addEventListener			("exit", function ()		{
-			_getFeePayable									();
-		});
-	});
 }
